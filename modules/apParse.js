e968b07b261d03b5c52b32e28dc3a2a963908474
Puffycheeses <noahheague@gmail.com>
2018-07-16 19:14:00 +1000

    Grabs all anime not just the first one

--- modules/apParse.js
+++ modules/apParse.js
@@ -1,16 +1,24 @@
 exports.parse = function ( $, response ) {
     console.log("Parsing");
     let tags = [];
+    let anime = [];
+    $('a[data-class="tooltip-full"]').each(function () {
+        let container = [];
+        container.push($(this).text());
+        container.push(`http://www.anime-planet.com${$(this).attr('href')}`);
+        anime.push(container)
+    });
     $('.tags ul li').each(function () { // Grab Tags first and make a nice array
         tags.push($(this).text())
+
     });
+
     let json = JSON.stringify({ // Grab elements from character page and set to json
         name: $("h1[itemprop=name]").text(),
         gender: $("div:contains('Gender:') .pure-1").html().split(":").pop(),
         image: "http://www.anime-planet.com" + $(".screenshots").attr('src'),
         rank: $("a[href$='/characters/top-loved']").text().split("#").pop(),
-        anime: $("a[data-class='tooltip-full'] ").first().text(),
-        animeUrl: "http://www.anime-planet.com" + $("a[data-class='tooltip-full'] ").first().attr('href'),
+        anime: anime,
         tags: tags
     });
     console.log("Responding");
